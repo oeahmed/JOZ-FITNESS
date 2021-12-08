bdc42285b09fb9bc66bac2b342776f33ff3d905c
Omar Ahmed <oeahmed@cougarnet.uh.edu>
2021-12-08 20:37:23 +0000

    Final Product w/out device

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+console.log('Hello from application.js')
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
