f25613b939122a8be23512ddec8bbbc93db82ce2
Jesus H <jshernandez3@uh.edu>
2021-12-09 19:23:45 +0000

    production files public.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.serve_static_files = true
+  config.serve_static_assets = true
+  config.public_file_server.enabled = true
 end
